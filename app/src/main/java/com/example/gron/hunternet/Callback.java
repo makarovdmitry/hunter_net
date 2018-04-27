aac28fcdcf4b82ad91117e8afec761e76c6b5c5f
Denis Makarov <d.makarov@simbirsoft.com>
2018-04-27 14:23:41 +0400

    Комментарии

--- app/src/main/java/com/example/gron/hunternet/Callback.java
+++ app/src/main/java/com/example/gron/hunternet/Callback.java
@@ -1,5 +1,6 @@
 package com.example.gron.hunternet;
 
+// TODO коллбек - это то, у чего обычно методы вида onSomething
 public interface Callback {
     void applyDataToActivity();
     void finishSaveImageProfile();
